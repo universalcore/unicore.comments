cb7d82f90939119cf048bcf25db350a2be58f0ff
Rizmari Versfeld <rizziepit@gmail.com>
2015-03-27 16:50:10 +0200

    more complete comments table + migrations

--- unicore/comments/service/tables.py
+++ unicore/comments/service/tables.py
@@ -1,12 +1,18 @@
 from uuid import uuid4
 
 from sqlalchemy import (Column, Integer, Unicode, MetaData, Table, Index,
-                        DateTime, ForeignKey)
-from sqlalchemy_utils import UUIDType
+                        DateTime, ForeignKey, Boolean)
+from sqlalchemy_utils import UUIDType, URLType
 
 
 COMMENT_MAX_LENGTH = 3000
 COMMENT_TABLE_NAME = 'comments'
+COMMENT_MODERATION_STATES = (
+    (u'visible', u'Visible'),
+    (u'removed_by_moderator', u'Removed by moderator'),
+    (u'removed_by_community', u'Removed by community'),
+    (u'removed_for_profanity', u'Removed for profanity'))
+
 FLAG_TABLE_NAME = 'flags'
 
 
@@ -26,10 +32,14 @@ comments = Table(
     Column('submit_datetime', DateTime, nullable=False),
     Column('content_type', Unicode(255), nullable=False),
     Column('content_title', Unicode(255), nullable=False),
+    Column('content_url', URLType(), nullable=False),
     Column('locale', Unicode(6), nullable=False),
+    Column('flag_count', Integer, default=0, nullable=False),
+    Column('is_removed', Boolean, default=False, nullable=False),
+    Column(
+        'moderation_state', Unicode(255), default='visible', nullable=False),
     # Not required data
     Column('ip_address', Unicode(15)),
-    Column('flag_count', Integer, default=0),
     # Indexes
     Index('comment_app_content_index', 'app_uuid', 'content_uuid'),
     Index('comment_user_index', 'user_uuid'),
