2164d4d9868ac2680b5b900318725bdb5eff4040
Rizmari Versfeld <rizziepit@gmail.com>
2015-03-30 13:34:12 +0200

    fix alembic env

--- unicore/comments/service/alembic/env.py
+++ unicore/comments/service/alembic/env.py
@@ -3,7 +3,7 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
-from unicore.comments.service import tables
+from unicore.comments.service import models
 
 
 # this is the Alembic Config object, which provides
@@ -18,7 +18,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = tables.metadata
+target_metadata = models.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
