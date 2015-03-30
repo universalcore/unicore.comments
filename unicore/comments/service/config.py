570a8042e23c5a381bec8bc12da4d8bb2a4b9aa7
Rizmari Versfeld <rizziepit@gmail.com>
2015-03-30 13:09:22 +0200

    get rid of nasty config creation

--- unicore/comments/service/config.py
+++ unicore/comments/service/config.py
@@ -8,7 +8,3 @@ class Config(ConfigBase):
     database_url = ConfigText(
         'The URL specifying the database to use',
         required=True)
-
-
-with open('config.yaml') as f:
-    config = Config(yaml.load(f))
